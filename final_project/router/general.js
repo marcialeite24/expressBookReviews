81c68272a4305131f7b4a8e4df3850003707ab15
Marcia Leite <marciasilvaleite@hotmail.com>
2024-11-05 06:07:48 -0500

    Get book by author & by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,14 +23,34 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let filtered_books = Object.entries(books)
+        .filter(([isbn, book]) => book.author === author)
+        .map(([isbn, book]) => ({
+            isbn: isbn,
+            title: book.title,
+            reviews: book.reviews
+        }));
+    const response = {
+        booksByAuthor: filtered_books
+    };
+    res.send(response);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filtered_books = Object.entries(books)
+        .filter(([isbn, book]) => book.title === title)
+        .map(([isbn, book]) => ({
+            isbn: isbn,
+            author: book.author,
+            reviews: book.reviews
+        }));
+    const response = {
+        booksByTitle: filtered_books
+    };
+    res.send(response);
 });
 
 //  Get book review
