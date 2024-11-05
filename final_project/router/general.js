fe6d1d39d06d2b1a9ef2c240201b24520b8a2018
Marcia Leite <marciasilvaleite@hotmail.com>
2024-11-05 06:30:12 -0500

    Get book reviews by ISBN and register customer

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(username && password) {
+        if(!isValid(username)) {
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "Customer successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "Customer already exists!"});
+        }
+    }
+    return res.status(404).json({ message: "Unable to register customer."});
 });
 
 // Get the book list available in the shop
@@ -55,8 +65,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
