cb21501ffa33e7c8b73473b354fcc121f20df734
Marcia Leite <marciasilvaleite@hotmail.com>
2024-11-05 10:06:47 -0500

    Add/Update and delete book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -50,8 +50,34 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+            filtered_book['reviews'][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }  
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let reviewer = req.session.authorization['username'];
+        delete filtered_book['reviews'][reviewer];
+        res.send(`Reviews for the ISBN  ${isbn} posted by the user ${reviewer} deleted.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }  
 });
 
 module.exports.authenticated = regd_users;
